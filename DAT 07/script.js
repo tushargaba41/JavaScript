647896031904bf1ffde146c95c6ade8ee60f031a
tushargaba41 <gabatushar1010@gmail.com>
2026-02-10 10:02:25 +0530

    Day 7: added DAT 07 folder with practice files

---
+++ DAT 07/script.js
@@ -0,0 +1,51 @@
+//Execution context
+var a=10;
+console.log(a);
+function demo(){
+    let b=20;
+    console.log(b);
+}
+demo();
+
+
+let user={
+    fullname:"Tushar gaba",
+    address:{
+        city:"palwal",
+        State:"Haryana",
+    },
+    mobileno:7206393909,
+    favcolor:["black","white","blue"],
+    demo:function(){
+        return "demo function"
+    }
+}
+// console.log(user.fullname,user.mobileno,user.favcolor[1], user.address.city,user.demo());
+
+// console.log("My name is "+user.fullname+". My fav color is "+user.favcolor[0]);
+
+// string literal
+// console.log(`My name is ${user.fullname}. My fav color is ${user.favcolor[0]}`);
+
+// Object methods
+
+// Object.keys(variable name)
+
+// console.log(Object.keys(user));
+
+// console.log(Object.values(user));
+
+// console.log(Object.entries(user));
+
+// const car = { 
+//     make: "Mahindra", 
+//     model: "Thar" 
+// };
+//
+// Object.freeze(car); //we cant add a new key and value pair and we cant change or update the existing value
+
+// Object.seal(car) // we cant add a new key and value pair but we can change and update the existing value
+
+// car.model="XUV700"
+// car.color = "zblack"; 
+// console.log(car);
